4e6bd48a8f89cf8bdb64431e4a2c040ddc879e2f
Sanaz Mohammadi <smohammadi.sdstudio@gmail.com>
2023-12-12 15:35:12 +0330

    Replace app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import time
+import io
 import torch
 import argparse
 import logging
@@ -11,15 +12,14 @@ import json
 
 import PIL
 from PIL import Image, ImageOps
-from io import BytesIO
 from fastapi import FastAPI, Form
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import HTTPException, Response
+from fastapi import HTTPException, Response, UploadFile
 import torch
 import numpy as np
 
-from modules.segmentation import Segmentor
+from modules.segmentation import Segmentor, NumpyEncoder
 
 
 PIL.Image.MAX_IMAGE_PIXELS = 933120000
@@ -31,40 +31,14 @@ resolution = {
     'high': 1024
 }
 
-def setup_minio(args):
-    from minio import Minio
-
-    min_client = Minio(
-        args.minioserver,
-        access_key=args.miniouser,
-        secret_key=args.miniopass,
-        secure=args.miniosecure  # Set to False if using an insecure connection (e.g., for local development)
-    )
-
-    return min_client
-
-
-@torch.inference_mode()
-def inference(img_org):
-    seg_image, sam_dict = segmentor.run_sam(img_org, segmentor, anime_style_chk=False)
-    return seg_image, sam_dict
-
-
 app = FastAPI()  # cal this object by uvicorn api
-
-
-class NumpyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return json.JSONEncoder.default(self, obj)
-    
 
 origins = [
     # "http://192.168.1.39:1025"
     # "http://localhost",
     # "http://localhost:1025",
 ]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -72,6 +46,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@torch.inference_mode()
+def inference(img_org):
+    seg_image, sam_dict = segmentor.run_sam(img_org, segmentor, anime_style_chk=False)
+    return seg_image, sam_dict
+
 
 def test_gpu_cuda():
     logging.info('test gpu and cuda:')
@@ -102,25 +83,12 @@ def read_image(object_name, timestamp, beforebucket, client):
 
     return img, local_before_path
 
-def runner(object_name: str,
-           res_mode: str, debug_mode: bool,
-           beforebucket: str, afterbucket: str, 
-           client, after_name: str):
+def runner(file, res_mode: str, debug_mode: bool):
     
-    timestamp = datetime.datetime.now().strftime('[%Y-%m-%d--%H-%M-%S]')
-    logging.info(timestamp)
-    
-    torch.cuda.empty_cache()
-    file_name, file_extension = os.path.splitext(object_name)
+    image_data = file.file.read()
+    img = Image.open(io.BytesIO(image_data))
 
-    temp_save_before_path = f'./{file_name}-{timestamp}'
-    local_before_path = temp_save_before_path + file_extension
-
-    get_image(beforebucket, object_name, local_before_path, client)
-
-    img = Image.open(local_before_path).convert("RGB")
-    img = ImageOps.exif_transpose(img)
-
+    segmentor.resize_image_with_height(img, resolution[res_mode])
     logging.info(f'\timage shape: {img.size}', )
     logging.info(f'\tstart inference')
     logging.info(f'\tresolution mode: {res_mode}')
@@ -132,51 +100,29 @@ def runner(object_name: str,
 
     logging.info(f'\tdone, time: {round(end - start, 4)}')
 
-    local_after_path = after_name
-    local_after_path = os.path.basename(local_after_path)
-    Image.fromarray(new_img).save(local_after_path)
-
-    # assert img.shape == enhanced_img.shape
-    put_image(afterbucket, after_name, local_after_path, client)
-
-    # remove local files
-    os.remove(local_before_path)
-    os.remove(local_after_path)
-
     torch.cuda.empty_cache()
     gc.collect()
 
-    if debug_mode:
-        # Create an in-memory file-like object
-        img_buffer = BytesIO()
 
-        # Save the PIL image to the in-memory buffer
-        Image.fromarray(new_img).save(img_buffer, format="JPEG")
+    output_dict = {}
+    for item in sam_dict['sam_masks']:
+        true_coords = np.argwhere(item['segmentation'])
+        area_number = item['area']
+        output_dict[area_number] = true_coords.tolist()
+        # arr_bytes = true_coords.tobytes()
+        # arr_base64 = base64.b64encode(arr_bytes)
+        # output_dict[area_number] = arr_base64
 
-        # Set the appropriate media type for the image
-        media_type = "image/jpeg"  # Adjust this based on your image format
-        img_buffer.seek(0)
+    json_dump = json.dumps(output_dict, cls=NumpyEncoder)
+    return json_dump
 
-        return StreamingResponse(img_buffer, media_type=media_type)
-    else:
-        output_dict = {}
-        for item in sam_dict['sam_masks']:
-            true_coords = np.argwhere(item['segmentation'])
-            area_number = item['area']
-            output_dict[area_number] = true_coords.tolist()
-
-        json_dump = json.dumps(output_dict, cls=NumpyEncoder)
-        return json_dump
 
 @app.post("/compute_sam/")
-async def compute_sam(object_name: str, 
-                             res_mode: str = 'high', 
-                             beforebucket: str = '',
-                             afterbucket: str = '',
-                             debug_mode: bool = False,
-                             after_name: str = '',
-                             env: str = ''
-                             ):
+async def compute_sam(file: UploadFile,
+                      res_mode: str = 'high', 
+                      debug_mode: bool = False,
+                      env: str = ''
+        ):
     """
         Perform computation and controlnet processing for an image.
 
@@ -200,33 +146,19 @@ async def compute_sam(object_name: str,
     If debug_mode is enabled, it returns a streaming response of the processed image for debugging purposes.
     """
     try:
-        args.miniosecure = bool(os.getenv(f'{env}_MINIO_SECURE'))
-        args.miniouser = os.getenv(f'{env}_MINIO_ACCESS_KEY')
-        args.miniopass = os.getenv(f'{env}_MINIO_SECRET_KEY')
-        args.minioserver = os.getenv(f'{env}_MINIO_ADDRESS')
 
-        args.minioserver = "192.168.32.33:9000"
-        args.miniouser = "test_user_chohfahe7e"
-        args.miniopass = "ox2ahheevahfaicein5rooyahze4Zeidung3aita6iaNahXu"
-        args.miniosecure = False
-
-        client = setup_minio(args)
         loop = asyncio.get_event_loop()
         response = await loop.run_in_executor(
                     None,
                     runner,
-                    object_name,
+                    file,
                     res_mode,
                     debug_mode, 
-                    beforebucket,
-                    afterbucket,
-                    client,
-                    after_name
                     )
 
         logging.info("\tINFO: POST /compute_sam HTTP/1.1 200 OK")
 
-        return response
+        return response 
 
     except Exception as e:
         torch.cuda.empty_cache()
@@ -291,7 +223,6 @@ if __name__ == "__main__":
     global segmentor
 
     sam_checkpoint = '../../models/sam_vit_h_4b8939.pth'
-    import pudb; pu.db
     segmentor = Segmentor(sam_checkpoint, 'cuda')
     logging.info('\model segmentation is loaded.')
 
