6d39a18d9807723d3ce399347818abc411e9eecf
sanaz mohammadi <smohammadi.sdstudio@gmail.com>
2023-12-25 17:06:46 +0330

    remove extra

--- modules/fast_sam/__init__.py
+++
@@ -1,9 +0,0 @@
-from .fast_sam_wrapper import FastSAM
-from .fast_sam_wrapper import FastSamAutomaticMaskGenerator
-
-fast_sam_model_registry = {
-    "FastSAM-x": FastSAM,
-    "FastSAM-s": FastSAM,
-}
-
-__all__ = ["FastSAM", "FastSamAutomaticMaskGenerator", "fast_sam_model_registry"]
